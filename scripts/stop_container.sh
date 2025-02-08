0dedc62c085f0fdd182911d074f9af0a5d9b40b2
shubham64mehta <shubham64mehta@gmail.com>
2025-02-08 14:15:36 +0530

    printing hi

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,13 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-CONTAINER_NAME="my_container"
-
-if docker ps -q --filter "name=$CONTAINER_NAME" | grep -q .; then
-    docker stop "$CONTAINER_NAME"
-    docker rm "$CONTAINER_NAME"
-fi
-
-
+echo hi
 
 # Replace <container_name> with the actual name of your container
