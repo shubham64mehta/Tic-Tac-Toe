18cdd61063be29b93a46542edbe5a940f51fdcbd
shubham64mehta <shubham64mehta@gmail.com>
2025-03-02 18:30:21 +0530

    updated stopcontainer

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# set -e
-# set -x
-# set -o pipefail
-
-# echo "Stopping the running container (if any)"
+set -e
 
 # Stop the running container (if any)
-# containerId = `docker ps |awk -f " " '{print $1}'`
-# docker rm -f $containerId
+echo "Hi"
