caa2b806514db5c645b33d8b678f265363f7b0f5
shubham64mehta <shubham64mehta@gmail.com>
2025-03-02 18:53:35 +0530

    updated the

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-containerId = sudo docker ps | awk 'NR>1 {print $1}'
-docker rm -f $containerId
+# Find the container using port 8000
+CONTAINER_ID=$(docker ps -q --filter "publish=8000")
+
+# Check if a container is running on port 8000
+if [ -n "$CONTAINER_ID" ]; then
+    echo "Stopping existing container on port 8000..."
+    docker stop "$CONTAINER_ID"
+    docker rm "$CONTAINER_ID"
+else
+    echo "No container is running on port 8000."
+fi
