3964cc4dd15710f7511166d2788d618c9625fc23
shubham64mehta <shubham64mehta@gmail.com>
2025-03-02 18:13:44 +0530

    commented the code in stop

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
-set -x
-set -o pipefail
+# set -e
+# set -x
+# set -o pipefail
 
-echo "Stopping the running container (if any)"
+# echo "Stopping the running container (if any)"
 
 # Stop the running container (if any)
 # containerId = `docker ps |awk -f " " '{print $1}'`
